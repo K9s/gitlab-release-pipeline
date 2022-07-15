7f2bd02e04124f37776117be03faf849b5064b93
Matthew Kennedy <matthew.kennedy@blueowl.xyz>
2022-07-15 14:31:00 -0700

    release.py: Better parse_version handling  - Move to class method  - Fixup some pylint warnings  - Remove unused devenv method

--- release.py
+++ release.py
@@ -4,6 +4,7 @@ import os
 import sys
 from copy import deepcopy
 from typing import Union
+import functools
 
 import fire
 import git
@@ -11,42 +12,18 @@ import git
 from packaging.version import parse as parse_semver
 from packaging.version import Version
 
-import functools
-
 sys.setrecursionlimit(10000)
 
 
-def _parse_version(self, version: Union[Version, str]):
-    if isinstance(version, Version):
-        _version = version
-    else:
-        version_str = str(str(version).
-                          replace(self.tag_prefix, '').
-                          replace('_', '+').
-                          replace('bump-', ''))
-
-        for tag_cleanup in [x for x in os.getenv('RP_RC_TAG_FIXUP_SUFFIXES', '').split(',') if x]:
-            version_str = version_str.replace(tag_cleanup, 'rc')
-
-        version_str = version_str.strip('.').strip('-')
-
-        _version = parse_semver(version_str)
-
-    if not isinstance(_version, Version) and not str(version).endswith('stable'):
-        raise Exception(f'Unable to parse version: {_version} as Version')
-
-    return _version
-
-
-def parse_version(func):
+def _version(func):
     @functools.wraps(func)
-    def wrapper_parse_version(self, *args, **kwargs):
+    def wrapper_version(self, *args, **kwargs):
         if kwargs.get('version', None):
-            kwargs['version'] = _parse_version(self, version=kwargs['version'])
+            kwargs['version'] = self.parse_version(version=kwargs['version'])
 
         return func(self, *args, **kwargs)
 
-    return wrapper_parse_version
+    return wrapper_version
 
 
 class Release:
@@ -57,15 +34,14 @@ class Release:
         self.recursion_depth = 0
 
         self.current_version_depth = 0
-        self._current_version = None
 
         self.tag_prefix = os.getenv('RP_TAG_PREFIX', '{self.app}-').strip('"').strip("'").format(self=self)
 
         self.build = 1
 
-        self.tags = {str(_parse_version(self, version=x.name)): {
+        self.tags = {str(self.parse_version(version=x.name)): {
             'tag': x,
-            'version': _parse_version(self, version=x.name),
+            'version': self.parse_version(version=x.name),
             'is_bump': x.name.startswith('bump-')}
             for x in self.repo.tags if x.name.replace('bump-', '').startswith(self.tag_prefix)}
 
@@ -82,7 +58,30 @@ class Release:
                 version_tags[str(tag['tag'].commit)].append(tag['version'])
         self.version_tags = {commit: sorted(versions) for commit, versions in version_tags.items() if versions}
 
-    @parse_version
+        self.current_version = self.get_version()
+
+    def parse_version(self, version: Union[Version, str]):
+        if isinstance(version, Version):
+            ver = version
+        else:
+            version_str = str(str(version).
+                              replace(self.tag_prefix, '').
+                              replace('_', '+').
+                              replace('bump-', ''))
+
+            for tag_cleanup in [x for x in os.getenv('RP_RC_TAG_FIXUP_SUFFIXES', '').split(',') if x]:
+                version_str = version_str.replace(tag_cleanup, 'rc')
+
+            version_str = version_str.strip('.').strip('-')
+
+            ver = parse_semver(version_str)
+
+        if not isinstance(ver, Version) and not str(version).endswith('stable'):
+            raise Exception(f'Unable to parse version: {ver} as Version')
+
+        return ver
+
+    @_version
     def get_versions(self,
                      bump: str = 'major',
                      version: Union[Version, str] = '0.0.0'):
@@ -102,7 +101,7 @@ class Release:
 
         return deepcopy(_versions)
 
-    @parse_version
+    @_version
     def get_latest_version(self,
                            bump: str = 'major',
                            version: Union[Version, str] = '0.0.0'):
@@ -120,12 +119,6 @@ class Release:
                 _versions.append(Version(f"0.0.0+{self.build}"))
 
         return sorted(_versions)[-1]
-
-    @property
-    def current_version(self):
-        if self._current_version is None:
-            self._current_version = self.get_version()
-        return self._current_version
 
     def get_version(self,
                     target: Version = None,
@@ -178,8 +171,9 @@ class Release:
             version = sorted(_register.keys())[-1]
             self.current_version_depth = _register[version]
             return version
+        return None
 
-    @parse_version
+    @_version
     def get_next_version(self,
                          bump: str = 'patch',
                          version: Union[Version, str] = '0.0.0'):
@@ -207,7 +201,7 @@ class Release:
 
         return next_version
 
-    @parse_version
+    @_version
     def get_bump_version(self,
                          bump: str = 'patch',
                          version: Union[Version, str, None] = None):
@@ -216,7 +210,7 @@ class Release:
 
         versions = self.get_versions(bump=bump, version=self.get_next_version(bump=bump, version=version))
 
-        if versions and not os.getenv('RP_LATEST_TAGGED_ANCESTOR_IGNORED', False):
+        if versions and not os.getenv('RP_LATEST_TAGGED_ANCESTOR_IGNORED'):
             latest_tagged_version = self.get_latest_version(bump=bump, version=versions[-1])
             ancestor_version = self.get_version(target=latest_tagged_version)
             if ancestor_version == Version('0.0.0rc0'):
@@ -224,7 +218,7 @@ class Release:
                                        f'unless {latest_tagged_version} (Latest tagged version) is an ancestor.  '
                                        f'You probably need to rebase/branch from latest tagged version.')
 
-        if version in versions and not os.getenv('RP_IGNORE_ALREADY_TAGGED', False):
+        if version in versions and not os.getenv('RP_IGNORE_ALREADY_TAGGED'):
             raise EnvironmentError(f'Unable to {bump} bump {self.app}. Version {version} has already been tagged!')
 
         versions.append(version)
@@ -245,28 +239,15 @@ class Release:
         return versions[-1]
 
 
-def dotenv(version: Version, release):
-    lines = [
-        f'VERSION={version.public}',
-        f'CURRENT_VERSION={release.current_version}',
-        f'RELEASE_SEMVER={version}',
-        f'VERSION_MAJOR={version.major}',
-        f'VERSION_MINOR={version.minor}',
-        f'TAG_PREFIX={release.tag_prefix}'
-    ]
-    with open('semver.env', 'w') as f:
-        f.writelines(line + '\n' for line in lines if line)
-
-
 if __name__ == "__main__":
     release = Release('.', app=os.getenv('APP'))
 
     SEMVER = os.getenv('SEMVER')
 
-    BUILD = 1 if os.getenv("BUILD") == '' else os.getenv("BUILD", 1)
+    BUILD = "1" if os.getenv("BUILD") == '' else os.getenv("BUILD", "1")
 
     if SEMVER:
-        __version = _parse_version(release, version=SEMVER)
+        __version = release.parse_version(version=SEMVER)
         release.build = __version.local if __version.local else BUILD
     else:
         VERSION = os.getenv('VERSION')
@@ -274,9 +255,9 @@ if __name__ == "__main__":
             VERSION = None
 
         if VERSION:
-            VERSION = _parse_version(release, version=VERSION)
+            VERSION = release.parse_version(version=VERSION)
             if not VERSION.local:
-                VERSION = _parse_version(release, f'{VERSION.public}+{BUILD}')
+                VERSION = release.parse_version(version=f'{VERSION.public}+{BUILD}')
 
             if os.getenv("BUILD") and os.getenv("BUILD") != VERSION.local:
                 raise EnvironmentError(
@@ -301,6 +282,5 @@ if __name__ == "__main__":
         'get-major': lambda: __version.major,
         'get-patch': lambda: __version.micro,
         'get-tag-prefix': lambda: release.tag_prefix,
-        'current-version-depth': lambda: release.current_version_depth,
-        'gen-dotenv': lambda: dotenv(__version, release)
+        'current-version-depth': lambda: release.current_version_depth
     })
